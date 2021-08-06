ca800c55322eea6a87f5fe81933713fce071a03d
js-rushing <unoriginal9000@yahoo.com>
2021-08-06 15:32:00 -0500

    Fix Heroku postgres uri issue

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ gravatar = Gravatar(app, size=100, rating='g', default='retro',
                     force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+db_uri = os.environ.get("DATABASE_URI", "sqlite:///blog.db")
+if db_uri.startswith("postgres://"):
+    db_uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
